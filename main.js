2df46732b132cf23d541983f1d59d892a47a85e9
nils-pettersson <nils-pettsson@outlook.com>
2020-06-14 13:21:10 +0200

    changed js function to classes and added modals to projects

--- main.js
+++ main.js
@@ -3,112 +3,17 @@ window.onload = function(){
     var learned = "from creating this project I learned how to do stuff and that will help me with stuff!";
     var skills = ["JavaScript", "C#", "C#", "asp.net"];
     var features = ["good something", "more of tha something", "good more something yes good", "cool new features and stuff", "new thing yes good!"];
-    createProject("projects/test.jpeg", "Project manager", descripton, learned, skills, features);
-    createProject("projects/test.jpeg", "Project manager", descripton, learned, skills, features);
+
+    var modal = new Modal("first modal");
+    var project = new Project(modal, "projects/test.jpeg", "Project manager", descripton, learned, skills, features);
+    
+    var modal2 = new Modal("second modal");
+    var project2 = new Project(modal2, "projects/test.jpeg", "Project manager", descripton, learned, skills, features);
+    
+
 }
 
 
-
-function createProject(imageSrc, title, descripton, learned, skills, features){
-    var project = document.createElement("div");
-    project.classList.add("project");
-
-    //div left
-    var leftDiv = document.createElement("div");
-    leftDiv.classList.add("project-left");
-    
-    //title
-    var descTitle = document.createElement("h1");
-    descTitle.innerHTML = title;
-    leftDiv.appendChild(descTitle);
-
-
-    //image
-    var imageDiv = document.createElement("div");
-    imageDiv.classList.add("image");
-
-    var img = document.createElement("img");
-    img.src = imageSrc;
-    imageDiv.appendChild(img);
-
-    leftDiv.appendChild(imageDiv);
-
-
-    //buttons
-    var demo = document.createElement("a");
-    demo.classList.add("button");
-    demo.innerHTML = "View project";
-    demo.href = "#";
-    leftDiv.appendChild(demo);
-
-    var code = document.createElement("a");
-    code.classList.add("button");
-    code.innerHTML = "View code";
-    code.href = "#";
-    leftDiv.appendChild(code);
-
-    //skills
-    var skillsTitle = document.createElement("h3");
-    skillsTitle.innerHTML = "Technologies Used";
-
-    var skillsDiv = document.createElement("div");
-    skillsDiv.classList.add("skills");
-    for(var i = 0; i < skills.length;i++){
-        var skill = document.createElement("a");
-        skill.innerHTML = skills[i];
-        skillsDiv.appendChild(skill);
-    }
-    leftDiv.appendChild(skillsTitle);
-    leftDiv.appendChild(skillsDiv);
-
-
-    //description
-    var desc = document.createElement("div");
-    desc.classList.add("project-desc");
-
-    var overviewTitle = document.createElement("h3");
-    overviewTitle.innerHTML = "Overview";
-    desc.appendChild(overviewTitle);
-
-    var overview = document.createElement("p");
-    overview.innerHTML = descripton;
-    desc.appendChild(overview);
-
-    //text
-    var descDesc = document.createElement("p");
-    descDesc.innerHTML = descripton;
-    desc.appendChild(descDesc);
-
-    //learned
-    var descLearned = document.createElement("h3");
-    descLearned.innerHTML = "What I learned";
-    desc.appendChild(descLearned);
-
-    var descLearnedText = document.createElement("p");
-    descLearnedText.innerHTML = learned;
-    desc.appendChild(descLearnedText);
-
-    var featuresDiv = document.createElement("div");
-    featuresDiv.classList.add("features");
-
-    var featureTitle = document.createElement("h3");
-    featureTitle.innerHTML = "Notable Features";
-    featuresDiv.appendChild(featureTitle);
-
-    for(var i = 0; i < features.length;i++){
-        var feature = document.createElement("li");
-        feature.innerHTML = features[i];
-        featuresDiv.appendChild(feature);
-    }
-    desc.appendChild(featuresDiv);
-
-    //add to project
-    project.appendChild(leftDiv);
-    project.appendChild(desc);
-    
-    
-    document.getElementById("second").appendChild(project);
-}
 
 window.smoothScroll = function(target) {
     var scrollContainer = target;
@@ -131,4 +36,141 @@ window.smoothScroll = function(target) {
     }
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+class Modal{
+    constructor(text){
+        this.modal = document.createElement("div");
+        this.modal.classList.add("modal");
+
+        var modalContent = document.createElement("div");
+        modalContent.classList.add("modal-content");
+
+        this.close = document.createElement("span");
+        this.close.classList.add("close");
+        this.close.innerHTML = "&times;";
+
+        var p = document.createElement("p");
+        p.innerHTML = text;
+
+        modalContent.appendChild(this.close);
+        modalContent.appendChild(p);
+        
+        this.modal.appendChild(modalContent);
+
+        document.body.appendChild(this.modal);
+    }
+}
+
+
+class Project{
+    constructor(modal, imageSrc, title, descripton, learned, skills, features){
+        this.project = document.createElement("div");
+        this.project.classList.add("project");
+
+        //div left
+        var leftDiv = document.createElement("div");
+        leftDiv.classList.add("project-left");
+        
+        //title
+        var descTitle = document.createElement("h1");
+        descTitle.innerHTML = title;
+        leftDiv.appendChild(descTitle);
+
+
+        //image
+        var imageDiv = document.createElement("div");
+        imageDiv.classList.add("image");
+
+        var img = document.createElement("img");
+        img.src = imageSrc;
+        imageDiv.appendChild(img);
+
+        leftDiv.appendChild(imageDiv);
+
+
+        //buttons
+        this.demo = document.createElement("a");
+        this.demo.classList.add("button");
+        this.demo.innerHTML = "View project";
+        leftDiv.appendChild(this.demo);
+
+        var code = document.createElement("a");
+        code.classList.add("button");
+        code.innerHTML = "View code";
+        code.href = "#";
+        leftDiv.appendChild(code);
+
+        //skills
+        var skillsTitle = document.createElement("h3");
+        skillsTitle.innerHTML = "Technologies Used";
+
+        var skillsDiv = document.createElement("div");
+        skillsDiv.classList.add("skills");
+        for(var i = 0; i < skills.length;i++){
+            var skill = document.createElement("a");
+            skill.innerHTML = skills[i];
+            skillsDiv.appendChild(skill);
+        }
+        leftDiv.appendChild(skillsTitle);
+        leftDiv.appendChild(skillsDiv);
+
+
+        //description
+        var desc = document.createElement("div");
+        desc.classList.add("project-desc");
+
+        var overviewTitle = document.createElement("h3");
+        overviewTitle.innerHTML = "Overview";
+        desc.appendChild(overviewTitle);
+
+        var overview = document.createElement("p");
+        overview.innerHTML = descripton;
+        desc.appendChild(overview);
+
+        //text
+        var descDesc = document.createElement("p");
+        descDesc.innerHTML = descripton;
+        desc.appendChild(descDesc);
+
+        //learned
+        var descLearned = document.createElement("h3");
+        descLearned.innerHTML = "What I learned";
+        desc.appendChild(descLearned);
+
+        var descLearnedText = document.createElement("p");
+        descLearnedText.innerHTML = learned;
+        desc.appendChild(descLearnedText);
+
+        var featuresDiv = document.createElement("div");
+        featuresDiv.classList.add("features");
+
+        var featureTitle = document.createElement("h3");
+        featureTitle.innerHTML = "Notable Features";
+        featuresDiv.appendChild(featureTitle);
+
+        for(var i = 0; i < features.length;i++){
+            var feature = document.createElement("li");
+            feature.innerHTML = features[i];
+            featuresDiv.appendChild(feature);
+        }
+        desc.appendChild(featuresDiv);
+
+        //add to project
+        this.project.appendChild(leftDiv);
+        this.project.appendChild(desc);
+        
+        
+        document.getElementById("second").appendChild(this.project);
+
+
+        //click events for modal
+        this.demo.onclick = function(){
+            modal.modal.style.display = "block";
+        }
+    
+        modal.close.onclick = function() {
+            modal.modal.style.display = "none";
+        }
+    }
 }
