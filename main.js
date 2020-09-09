3276392a5753c8a7a7fdc63bb4abc3f97e905046
Nils Pettersson <nils-pettsson@outlook.com>
2020-09-09 11:43:16 +0200

    fixed bug and added indication of where in the page you are

--- main.js
+++ main.js
@@ -21,3 +21,80 @@ window.smoothScroll = function(target) {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+        findClosest(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function findClosest(scroll_pos) {
+    let target = document.getElementById("home");
+    let scrollContainer = target;
+    scrollContainer = scrollContainer.parentNode;
+    let homeY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        homeY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    target = document.getElementById("projects");
+    scrollContainer = scrollContainer.parentNode;
+    let portfolioY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        portfolioY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    target = document.getElementById("about");
+    scrollContainer = scrollContainer.parentNode;
+    let aboutY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        aboutY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    console.log(scroll_pos);
+    let home = Math.abs(homeY - scroll_pos);
+    let portfolio = Math.abs(portfolioY - scroll_pos);
+    let about = Math.abs(aboutY - scroll_pos);
+    if(home < portfolio && home < about){
+        let portfolio = document.getElementById("homeLink");
+        portfolio.classList.add("active");
+
+        element = document.getElementById("portfolioLink");
+        element.classList.remove("active");
+        element = document.getElementById("aboutLink");
+        element.classList.remove("active");
+    }
+    else if(portfolio < home && portfolio < about){
+        let element = document.getElementById("portfolioLink");
+        element.classList.add("active");
+
+        element = document.getElementById("homeLink");
+        element.classList.remove("active");
+        element = document.getElementById("aboutLink");
+        element.classList.remove("active");
+    }
+    else if(about < home && about < portfolio){
+        let element = document.getElementById("aboutLink");
+        element.classList.add("active");
+
+        element = document.getElementById("portfolioLink");
+        element.classList.remove("active");
+        element = document.getElementById("homeLink");
+        element.classList.remove("active");
+    }
+    
+    
+}
